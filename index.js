1837991257bd8043fc24d3d20f97868ef1a70f9e
Yvonne Wang <yvonnewang@d209-087-059-001.wireless.sfu.ca>
2023-11-17 15:30:41 -0800

    added more stuff

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express"); // this is the Sinatra-like MVC frameworks for Node.js
+// var cors = require("cors"); // cross-origon resourse sharing
+const path = require('path');
+
+// this is our app
+var app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+// app.get("/", (req, res) => res.render("pages/index"));
+// app.get("/", (req, res) => res.send("hello world"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
