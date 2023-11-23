f4747f25e596bc8d16a971994136507601fb5c18
Yvonne Wang <yvonnewang@d209-087-059-168.wireless.sfu.ca>
2023-11-22 21:11:21 -0800

    test again

--- public/acl.js
+++ public/acl.js
@@ -1,5 +1,5 @@
-// import { createRequire } from 'node:module';
-// const require = createRequire(import.meta.url);
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
 
 
 
