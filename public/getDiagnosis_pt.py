9b13cd08d52c88158c3c57202849a371b1480723
Yvonne Wang <yvonnewang@d142-058-043-149.wireless.sfu.ca>
2023-11-28 15:43:45 -0800

    changed colors a bit

---
+++ public/getDiagnosis_pt.py
@@ -0,0 +1,20 @@
+import torch
+import sys
+
+
+
+
+def main():
+    print(sys.argv[1]) # should be the plane
+
+    model_axial_acl = "models/cnn_axial_acl_02.pt"
+    model_axial_meniscus = "models/cnn_axial_meniscus_03.pt"
+
+
+
+
+    model = torch.jit.load('model_scripted.pt')
+    model.eval()
+
+if __name__ == '__main__':
+    main()
